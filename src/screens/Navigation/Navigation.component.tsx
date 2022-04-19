4b2a2660912a2f0a5635e4cd5cb2123e39c8bcee
Alexey Glazkov <alexey.glazkov@platoanalytics.com>
2022-04-19 20:16:41 +0300

    Define routes in separate files

--- src/screens/Navigation/Navigation.component.tsx
+++ src/screens/Navigation/Navigation.component.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import HomeScreen from '../Home';
+import * as ROUTES from 'Type/routes';
 import { NavigationProps } from './Navigation.config';
+
+const HomeScreen = lazy(() => import('../Home'));
 
 const Navigation: React.FC<NavigationProps> = ({ children }) => (
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomeScreen />} />
-      <Route path="/profile" element={<div>Profile</div>} />
-      <Route path="/direct" element={<div>Messages</div>} />
-    </Routes>
+    <Suspense fallback={<p>Loading ...</p>}>
+      <Routes>
+        <Route path={ROUTES.HOME} element={<HomeScreen />} />
+        <Route path={ROUTES.PROFILE} element={<div>Profile</div>} />
+        <Route path={ROUTES.DIRECT} element={<div>Messages</div>} />
+      </Routes>
+    </Suspense>
     { children }
   </BrowserRouter>
 );
